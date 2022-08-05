4d019d8168765c7ccc55eec2f8685a13bb924588
SahrulGnwn <shrlgnwn385@gmail.com>
2022-08-05 21:50:01 +0700

    Update api-list.blade.php

--- resources/views/dashboard/settings/api-list.blade.php
+++ resources/views/dashboard/settings/api-list.blade.php
@@ -18,7 +18,7 @@
 <!-- Default box -->
 <div class="card">
   <div class="card-header">
-    <h3 class="card-title"><span class="badge badge-primary">GET</span> [ /api/users ] User Details</h3>
+    <h3 class="card-title"><span class="badge badge-primary">GET</span> [ /api/user ] User Token Details</h3>
     <div class="card-tools">
       <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
         <i class="fas fa-minus"></i>
